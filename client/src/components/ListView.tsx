f118204fc0ebb7ba60f52d9d9610f371fa49fe34
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2021-11-01 19:56:33 -0400

    styling and refactor

--- client/src/components/ListView.tsx
+++ client/src/components/ListView.tsx
@@ -1,17 +1,29 @@
 import { useApp } from "../AppProvider";
+import { fire } from "../img";
 
 export default function ListView () {
   const app = useApp ();
 
+  if (!app.location.locationAvailable) return null;
   return (
-    <ul className="pin-list-view">
-      {
-        app.pins.distribution.map (({name, percent}) => (
-          <li>
-            {name}
-          </li>
-        ))
-      }
-    </ul>
+    <div className="pin-list-view">
+      <ul>
+        <li>
+          <span>
+            <img src={fire} />
+          </span>
+          <span><h2>Top 5</h2></span>
+        </li>
+        {
+          app.pins.distribution.filter ((_, i) => i < 5).map (({name}, i) => (
+            <li>
+              <span>{i + 1}.</span>
+              <span>{name}</span>
+            </li>
+          ))
+        }
+      </ul>
+      <span className="minimize">hide</span>
+    </div>
   );
 }
