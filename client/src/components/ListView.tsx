d1913ead5a6e39bceb6be5e764880ca4d4c9889d
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2021-11-01 20:10:22 -0400

    styling and refactor

--- client/src/components/ListView.tsx
+++ client/src/components/ListView.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { useApp } from "../AppProvider";
 import { fire } from "../img";
+
+interface Switcher {
+  [key: string]: string
+}
+
+const switcher:Switcher = {
+  'pin-list-view': 'pin-list-view hidden',
+  'pin-list-view hidden': 'pin-list-view'
+}
 
 export default function ListView () {
   const app = useApp ();
 
+  const [className, setClassName] = useState<string> ('pin-list-view');
+
   if (!app.location.locationAvailable) return null;
   return (
-    <div className="pin-list-view">
+    <div className={className} onClick={() => setClassName (switcher [className])}>
       <ul>
         <li>
           <span>
@@ -14,6 +26,7 @@ export default function ListView () {
           </span>
           <span><h2>Top 5</h2></span>
         </li>
+        <hr />
         {
           app.pins.distribution.filter ((_, i) => i < 5).map (({name}, i) => (
             <li>
